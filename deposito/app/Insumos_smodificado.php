3a15614e71dbb7236c3317ff0491462727171de5
Mario Gonzalez <pgmariodavid@gmail.com>
2015-12-02 13:13:52 -0430

    Crea tabla insumos_smodificaciones y modelo

---
+++ deposito/app/Insumos_smodificado.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Insumos_smodificado extends Model
+{
+    protected $fillable = ['salida','insumo','Osolicitado','Msolicitado','Odespachado', 'Mdespachado'];
+}
---
+++ deposito/database/migrations/2015_12_02_125805_create_table_insumos_smodificados.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableInsumosSmodificados extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('insumos_smodificados', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('salida');
+            $table->integer('insumo');
+            $table->double('Osolicitado');
+            $table->double('Msolicitado');
+            $table->double('Odespachado');
+            $table->double('Mdespachado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('insumos_smodificados');
+    }
+}
