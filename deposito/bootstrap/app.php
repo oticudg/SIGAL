81ea20409e203d36dfdc930758e3c6882e36b64a
Mario Gonzalez <pgmariodavid@gmail.com>
2016-02-24 15:59:01 -0430

    Agrega header no cache

--- deposito/bootstrap/app.php
+++ deposito/bootstrap/app.php
@@ -11,6 +11,10 @@
 |
 */
 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
