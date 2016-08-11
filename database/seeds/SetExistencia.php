17f002dee245d276818d39cc736468beddadbbe3
Mario Gonzalez <pgmariodavid@gmail.com>
2016-08-11 14:59:46 -0400

    Modifica seeder SetExistencia.

--- database/seeds/SetExistencia.php
+++ database/seeds/SetExistencia.php
@@ -34,30 +34,54 @@ class SetExistencia extends Seeder
   										 ->orderBy('created_at','asc')
   										 ->get();
 
-  				$existencia = 0;
+  				$init_year_search = date('Y-01-01 00:00:00',strtotime('2016-01-01'));
 
   				foreach ($movimientos as $movimiento){
 
+            //Obtiene la fecha de la ultima carga de inventario realizada en el primer año del rango de fecha a consultar
+            $last_cinve = DB::table('entradas')
+                          ->where('deposito', $deposito)
+                          ->where('type','cinventario')
+                          ->whereBetween('created_at', [$init_year_search, $movimiento->created_at])
+                          ->orderBy('id', 'desc')
+                          ->value('created_at');
+            /**
+             *Si se ha encontrado una carga de inventario en el primer año del rango de fecha a consultar,
+             *construye consulta que obtienen todas las entradas y salidas desde la fecha de dicha carga de
+             *inventario hasta la fecha del primer movimiento encontrado, de lo contrario construye consulta
+             *que obtiene todas las entradas y salidas desde el primer año del rango de fecha a consultar
+             *hasta la fecha del primer movimiento encontrado.
+             */
+            if(!empty($last_cinve) ){
+              $queryE = DB::table('insumos_entradas')->where('insumo', $insumo)
+                     ->where('deposito', $deposito)
+                     ->whereBetween('created_at',[$last_cinve, $movimiento->created_at]);
+              $queryS = DB::table('insumos_salidas')->where('insumo', $insumo)
+                     ->where('deposito', $deposito)
+                     ->whereBetween('created_at',[$last_cinve, $movimiento->created_at]);
+            }
+
+            /**
+             *Realiza la consulta que Obtiene la cantidad de salidas y entradas
+             *de los movimientos de la consultas que se almacenan en $queryE, $queryS.
+             */
+            $entradaM = $queryE->sum('cantidad');
+            $salidaM  = $queryS->sum('despachado');
+            $existencia = $entradaM - $salidaM;
+
   					if( $movimiento->type == 'salida'){
-  						$existencia -= $movimiento->movido;
 
   						DB::table('insumos_salidas')->where('id', $movimiento->id)->update([
-  							'existencia' => $existencia
+  							'existencia' => round($existencia, 2)
   						]);
 
   						continue;
   					}
 
-  					if( $movimiento->type == 'cinventario')
-  						$existencia = $movimiento->movido;
-  					else
-  						$existencia += $movimiento->movido;
-
   					DB::table('insumos_entradas')->where('id', $movimiento->id)->update([
-  						'existencia' => $existencia
+  						'existencia' => round($existencia, 2)
   					]);
   				}
-
   			}
   		}
     }
